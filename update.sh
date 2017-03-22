1af880f4f9906d4cd42efe2225e0660dfe79ef28
Eugene Asiedu <ngene84@gmail.com>
2017-03-22 14:01:06 +0000

    Remove update script

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git pull
-
-if sudo docker stop web-instant; then sudo docker rm web-instant fi;
-sudo docker rmi web-instant
-sudo docker build -t web-instant .
-sudo docker run -d -p 3000:3000 --name web-instant web-instant
